e67aff8d6874fcbfbd566991787426c6c013cfdc
Ace Locker <lockerace@gmail.com>
2022-10-07 15:40:28 +0800

    update: login layout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="{{ route('login-submit') }}" method="post">
-    @csrf
-    <input name="no_anggota" placeholder="Nomor Anggota" />
-    <input name="password" placeholder="Password" type="password" />
-    <button>Login</button>
-</form>
+<div class="container">
+  <div class="col-md-8 card m-auto my-5">
+    <div class="card-header">Log In</div>
+    <div class="card-body">
+      <form action="{{ route('login-submit') }}" method="post">
+        @csrf
+        <div class="mb-3">
+          <label class="form-label">Nomor Anggota</label>
+          <input class="form-control" name="no_anggota" placeholder="Nomor Anggota" />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Password</label>
+          <input class="form-control" name="password" placeholder="Password" type="password" />
+        </div>
+        <button class="btn btn-primary">Login</button>
+      </form>
+    </div>
+  </div>
+</div>
 @endsection
